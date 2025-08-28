213147e01d74d0e2abcf855267ed57a6f7dd6739
nhittyn <hudinhdong@gmail.com>
2025-08-28 15:06:29 +0700

    add collectable items and test

--- scenes/components/collectable_component.gd
+++ scenes/components/collectable_component.gd
@@ -1,4 +1,7 @@
 class_name CollectableComponent extends Area2D
 
+@export var collectable_name: String
+
 func _on_body_entered(body: Player) -> void:
+	print("Collected: ", collectable_name)
 	get_parent().queue_free()
